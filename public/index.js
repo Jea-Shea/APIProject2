5c0c271f56ea731af7a3a58c6ecd087e33c26fb7
Jea-Shea <67469250+Jea-Shea@users.noreply.github.com>
2020-10-01 13:24:43 -0400

    updating for submission

--- public/index.js
+++ public/index.js
@@ -6,17 +6,21 @@ const searchTerm = document.querySelector('.search');
 const searchForm = document.querySelector('form'); 
 const submitBtn = document.querySelector('.submit');
 const colorTerm = document.querySelector('.color');
-const container = document.querySelector('#test');
+const container = document.querySelector('#container');
+const body = document.querySelector('body');
 searchForm.addEventListener('submit', wallpapers);
 
 
 
 async function wallpapers(e) {
     e.preventDefault();
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
     const search = searchTerm.value;
-    const color = colorTerm.value;
-    url = `${baseURL}query=${search}&color=${color}&client_id=${key}`;
-    await fetch(url) 
+    const color = colorTerm.value;                 //customized results
+    url = `${baseURL}query=${search}&color=${color}&per_page=30&orientation=landscape&client_id=${key}`;
+    await fetch(url)  //left on await for additional function
     .then( async function (result) {
         let results = await result.json();
         console.log(results) 
@@ -26,14 +30,11 @@ async function wallpapers(e) {
         console.log(json);
        displayWallpaper(json);
     })
+
 }
 
 function displayWallpaper(json)   {
 
-    //console.log('Display Results:', json);
-    /* while (container.firstChild) { //debating just doing the little clear loop at the end?
-        container.removeChild(container.firstChild);
-    } */
     let wallpapers = json.results;
 
 
@@ -41,20 +42,37 @@ function displayWallpaper(json)   {
         console.log('No results');
     } else { 
         for (let i = 0; i < wallpapers.length; i++) {
-            let images = document.createElement('img');
-            images.setAttribute('class', 'col-lg-4 col-md-6 mb-4'); 
+            //some users tag their images wrong so they pop up like ads
+            if (wallpapers[i].user.username == 'boxedwater' || wallpapers[i].user.username == "windows") continue; //continue operates like a "skip"
+            let link = document.createElement('a');
+            link.setAttribute('href', '')
+            link.href = wallpapers[i].links.download; //need to assign link to each result
+            container.appendChild(link);
 
-            let current = wallpapers[i].urls.small;
+
+
+
+            let images = document.createElement('img');
+            let current = wallpapers[i].urls.small; //then grab the images themselves
             console.log(current);
             images.src = current;
-            container.appendChild(images);
+            body.style.marginTop = '5em';
+            link.appendChild(images);
+            images.setAttribute('class', 'col-lg-4 col-md-6 mb-4'); 
 
-            searchForm.addEventListener('submit', () => {
-                console.log('oh hi mark')
-                container.removeChild(images);
-                wallpapers();
-            })
+
         }
     }
 
 }
+
+/* submitBtn.addEventListener('click', () => {
+    //consider adding a while loop from NYT to remove
+
+    displayWallpaper();
+})
+
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+} */
